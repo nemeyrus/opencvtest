f222bdb558f628fbf77d989856ae08890d8c35d5
nemeyrus <n.volkov@list.ru>
2024-05-25 22:19:45 +0300

    hi

---
+++ opencv.py
@@ -0,0 +1,11 @@
+import cv2
+image = cv2.imread("testfile.jpg")
+wide = 25
+f = float(wide) / image.shape[1]
+new_size = (wide,int(image.shape[0] * f))
+print(new_size)
+res = cv2.resize(image,new_size,interpolation=cv2.INTER_AREA)
+cv2.imshow("Resize image",res)
+crop = res[50:100, 150:250]
+cv2.imshow("CropImage",crop)
+cv2.waitKey(0)
