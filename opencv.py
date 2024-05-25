35a456cdecf16f73661de5580b7c53ee39457b27
nemeyrus <n.volkov@list.ru>
2024-05-25 23:17:01 +0300

    Update opencv.py

--- opencv.py
+++ opencv.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 import cv2
 image = cv2.imread("testfile.jpg")
 wide = 25
@@ -7,6 +7,6 @@ new_size = (wide,int(image.shape[0] * f))
 print(new_size)
 res = cv2.resize(image,new_size,interpolation=cv2.INTER_AREA)
 cv2.imshow("Resize image",res)
-crop = res[50:100, 150:250]
+crop = res[50:100, 150:200]
 cv2.imshow("CropImage",crop)
 cv2.waitKey(0)
